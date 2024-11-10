b0c2c8fa3f77aedfce12e966a147e0a76b6619e1
Nicolas Busso <nicolasbusso@gmail.com>
2024-11-10 17:10:38 -0300

    solved tempCoversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,28 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (degrees) {
+  let converted = ((degrees - 32) * 5) / 9;
+  if (!Number.isInteger(converted)) {
+    converted = parseFloat(converted).toFixed(1);
+  }
+
+  console.log(converted);
+  return Number(converted);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (degrees) {
+  let converted = degrees * (9 / 5) + 32;
+
+  if (!Number.isInteger(converted)) {
+    converted = parseFloat(converted).toFixed(1);
+  }
+
+  console.log(converted);
+  return Number(converted);
 };
+
+// well there's nothing like the solution lol...
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
