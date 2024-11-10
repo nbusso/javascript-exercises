6f72a748e14d6abe85a0dc34d92f098425ff23f2
Nicolas Busso <nicolasbusso@gmail.com>
2024-11-10 16:14:17 -0300

    solved sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function (from, to) {
+  let sum = 0;
+  let smol = from > to ? to : from;
+  let big = from > to ? from : to;
 
+  if (
+    isNaN(from) ||
+    isNaN(to) ||
+    !Number.isInteger(from) ||
+    !Number.isInteger(to) ||
+    from < 0 ||
+    to < 0
+  ) {
+    console.log("ERROR");
+    return "ERROR";
+  }
+  for (let i = smol; i <= big; i++) {
+    sum += i;
+  }
+
+  console.log(sum);
+  return sum;
 };
 
 // Do not edit below this line
