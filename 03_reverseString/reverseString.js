3865a55da12437159fcf36265ab2fe6f9c9d888f
Nicolas Busso <nicolasbusso@gmail.com>
2024-11-10 15:18:12 -0300

    solved reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
+const reverseString = function (string) {
+  let array = string.split("");
 
+  return array.reverse().join("");
 };
 
 // Do not edit below this line
