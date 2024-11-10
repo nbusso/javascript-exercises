9ff576cadda3ff5d730fbfdba75eba83bd590149
Nicolas Busso <nicolasbusso@gmail.com>
2024-11-10 15:52:03 -0300

    solved removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...args) {
+  let newArr = [];
+  let banned = args;
+
+  for (const i of arr) {
+    if (banned.includes(i)) continue;
+    newArr.push(i);
+  }
+  console.log(newArr);
+  return newArr;
 };
 
 // Do not edit below this line
